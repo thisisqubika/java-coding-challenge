0553d4247c4241f996c5e1e7a266d854ad595e8a
puma <jlrp39@gmail.com>
2019-12-31 13:43:00 -0500

    Cleaning Code

--- cars/src/main/java/com/mooveit/cars/tasks/FordIngesterTask.java
+++ cars/src/main/java/com/mooveit/cars/tasks/FordIngesterTask.java
@@ -5,15 +5,10 @@ import com.mooveit.cars.repositories.BrandRepository;
 import com.mooveit.cars.utils.XmlProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 
 @Slf4j
 @Service
@@ -21,15 +16,15 @@ public class FordIngesterTask {
 
     public static final String FORD = "FORD";
     @Autowired
-  private BrandRepository brandRepository;
+    private BrandRepository brandRepository;
 
-  @Autowired
-  private XmlProcessor xmlProcessor;
+    @Autowired
+    private XmlProcessor xmlProcessor;
 
-  @Scheduled(cron = "${cars.ford.ingester.runCron}")
-  public void ingestFile() {
-      Brand brand = brandRepository.findByName(FORD);
-      File file = new File(getClass().getClassLoader().getResource("ford-example.xml").getFile());
-      if(file.exists()) xmlProcessor.processFile(file, brand);
-  }
+    @Scheduled(cron = "${cars.ford.ingester.runCron}")
+    public void ingestFile() {
+        Brand brand = brandRepository.findByName(FORD);
+        File file = new File(getClass().getClassLoader().getResource("ford-example.xml").getFile());
+        if (file.exists()) xmlProcessor.processFile(file, brand);
+    }
 }
